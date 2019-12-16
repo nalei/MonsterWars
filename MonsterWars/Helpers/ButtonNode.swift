e2b86d7fbc9175abab7c4eecae9f43086472c2b0
Yuri Anikin <anikin.yu@gmail.com>
2019-12-16 23:50:24 +0300

    Initial Commit

---
+++ MonsterWars/Helpers/ButtonNode.swift
@@ -0,0 +1,46 @@
+import Foundation
+import SpriteKit
+
+class ButtonNode : SKSpriteNode {
+  
+  let onButtonPress: () -> ()
+  
+  init(iconName: String, text: String, onButtonPress: @escaping () -> ()) {
+    
+    self.onButtonPress = onButtonPress
+    
+    let texture = SKTexture(imageNamed: "button")
+    super.init(texture: texture, color: SKColor.white, size: texture.size())
+    
+    let icon = SKSpriteNode(imageNamed: iconName)
+    icon.position = CGPoint(x: -size.width * 0.25, y: 0)
+    icon.zPosition = 1
+    self.addChild(icon)
+    
+    let label = SKLabelNode(fontNamed: "Courier-Bold")
+    label.fontSize = 50
+    label.fontColor = SKColor.black
+    label.position = CGPoint(x: size.width * 0.25, y: 0)
+    label.zPosition = 1
+    label.verticalAlignmentMode = .center
+    label.text = text
+    self.addChild(label)
+    
+    isUserInteractionEnabled = true
+    
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    self.texture = SKTexture(imageNamed: "button_sel")
+    onButtonPress()
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    self.texture = SKTexture(imageNamed: "button")
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
---
+++ MonsterWars/Helpers/Constants.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+let costQuirk = 10
+let costZap = 25
+let costMunch = 50
